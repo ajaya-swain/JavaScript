c14398d6df648db673668a2a98eeea3a3c24fbe6
Ajaya Swain <150909889+ajaya-swain@users.noreply.github.com>
2024-02-08 04:59:50 +0000

    conversion operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33;
+let isLogin = 1;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+let valueInBoolean = Boolean(isLogin);
+let valueInString = String(score);
+
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+console.log(valueInBoolean);
+console.log(typeof(valueInBoolean));
+
+console.log(valueInString);
+console.log(typeof(valueInString));
+
