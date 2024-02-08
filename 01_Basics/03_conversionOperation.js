36e77cae81acde912e7eae7f4cefcac25eef0b1f
Ajaya Swain <150909889+ajaya-swain@users.noreply.github.com>
2024-02-08 08:49:15 +0000

    comparison & increment operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -8,12 +8,30 @@ let valueInNumber = Number(score);
 let valueInBoolean = Boolean(isLogin);
 let valueInString = String(score);
 
-console.log(valueInNumber);
-console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
 
-console.log(valueInBoolean);
-console.log(typeof(valueInBoolean));
+// console.log(valueInBoolean);
+// console.log(typeof(valueInBoolean));
 
-console.log(valueInString);
-console.log(typeof(valueInString));
+// console.log(valueInString);
+// console.log(typeof(valueInString));
 
+let str1 = "Hello"
+let str2 = "Ajaya"
+
+let str = str1 + str2;
+console.log(str);  
+
+//*************increment/decrement************ */
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
