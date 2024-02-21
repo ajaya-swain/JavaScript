ca22ee022f93e1c4bf583b2d6f1f550a2792aa59
ajaya-swain <ajayaswain212@gmail.com>
2024-02-21 20:30:25 +0530

    switch

---
+++ 04_Control_Flow/02_switch.js
@@ -0,0 +1,69 @@
+//*************switch************ */
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//********** */
+const week=2
+
+switch (week) {
+    case 1:
+        console.log("Sunday")
+        break;
+    case 2:
+        console.log("Monday")    //Monday
+        break;
+    case 3:
+        console.log("Tuesday")
+        break;
+    case 4:
+        console.log("Wednesday")
+        break;
+    case 5:
+        console.log("Thursday")
+        break;
+    case 6:
+        console.log("Friday")
+        break;
+    case 7:
+        console.log("Saturday")
+        break;
+    default:
+        console.log("Enter a valid Week")
+        break;
+}
+
+//********************** */
+const weekday="5"  //string type
+
+switch (weekday) {
+    case "1":
+        console.log("Sunday")
+        break;
+    case "2":
+        console.log("Monday")    
+        break;
+    case "3":
+        console.log("Tuesday")
+        break;
+    case "4":
+        console.log("Wednesday")
+        break;
+    case "5":
+        console.log("Thursday")   //Thursday
+        break;
+    case "6":
+        console.log("Friday")
+        break;
+    case "7":
+        console.log("Saturday")
+        break;
+    default:
+        console.log("Enter a valid Week")
+        break;
+}
