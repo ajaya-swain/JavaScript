4ea988b19d84d8341b324455c7f24b39b68f6cce
ajaya-swain <ajayaswain212@gmail.com>
2024-02-23 16:39:12 +0530

    reduce

---
+++ 05_iterations/08_reduce.js
@@ -0,0 +1,52 @@
+//***************reduce**************** */
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);  // Expected output: 10
+
+//************************************** */
+
+const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0)
+
+console.log(myTotal)    //6 
+
+//******************************************* */
+const myNums02 = [1, 2, 3]
+const myTotal02 = myNums02.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal02)   //6
+
+//************************************** */
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(`Total : ${priceToPay}`);  //Total : 22996
